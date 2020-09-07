1d25eb7eb959d39fd58f46921a71427a468202b4
Mark Vincent <markdeanilvicente@gmail.com>
2020-09-07 16:36:36 +0800

    initial codes

---
+++ App/ClassifyStrength.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CheckPasswordStrength
+{
+    public static class ClassifyStrength
+    {
+        public static StrengthProperty PasswordStrength(this string password)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(password))
+                throw new ArgumentNullException();
+
+            Debug.WriteLine("Test");
+
+            return new StrengthProperty();
+        }
+    }
+}
---
+++ App/StrengthProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckPasswordStrength
+{
+    public class StrengthProperty
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+        public int Length { get; set; }
+
+        public IList<Contain> Contains { get; set; }
+    }
+
+    public class Contain
+    {
+        public string Message { get; set; }
+    }
+}
